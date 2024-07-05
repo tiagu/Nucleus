abd5aa97347da469138d9553c037642da3bbc967
Tiago <19781147+tiagu@users.noreply.github.com>
2024-07-05 18:34:05 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+VERSION = "0.5"
+
+with open("README.md", encoding="UTF-8") as f:
+    readme = f.read()
+
+with open("requirements.txt", encoding="UTF-8") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="nucleus",
+    version=VERSION,
+    description="Nucleus detects individual nuclei in crowded tissue-like confocal microscopy images",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords=['segmentation','2D','cryosections','mask R-CNN','Detectron2'],
+    author="Tiago Rito",
+    author_email="tiago.rito@crick.ac.uk",
+    url="https://github.com/tiagu/Nucleus",
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=required,
+    packages=find_packages(exclude="notebooks"),
+    include_package_data=True,
+    zip_safe=False,
+)
